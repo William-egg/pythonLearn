b2cff1a6b1a5f03660315f6c7a655d05c6a43d9d
zhuang <814724008@qq.com>
2023-10-20 17:20:07 -0800

    second

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import torch
-
+# asdjakjds
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(torch.__version__)
